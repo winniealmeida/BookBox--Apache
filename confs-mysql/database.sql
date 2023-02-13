b3c889ca43faef7edc8632a788d2a5f61b511609
leticiamaiaa <79123176+leticiamaiaa@users.noreply.github.com>
2023-02-13 18:52:22 -0300

    Update database.sql

--- confs-mysql/database.sql
+++ confs-mysql/database.sql
@@ -1,18 +1,18 @@
 CREATE TABLE categorias
 (
-  id_categoria INT NOT NULL AUTO_INCREMENT,
+  id_categoria INTEGER NOT NULL AUTO_INCREMENT,
   nome_categoria VARCHAR(255) NOT NULL,
   PRIMARY KEY (id_categoria)
 );
 
 CREATE TABLE livros
 (
-  id_livro INT NOT NULL AUTO_INCREMENT,
+  id_livro INTEGER NOT NULL AUTO_INCREMENT,
   nome_livro VARCHAR(45) NOT NULL,
   editora_livro VARCHAR(255) NOT NULL,
   autor_livro VARCHAR(255) NOT NULL,
-  resenha_livro VARCHAR(Max) NOT NULL,
-  id_categoria INT NOT NULL,
+  resenha_livro VARCHAR(255) NOT NULL,
+  id_categoria INTEGER NOT NULL,
   PRIMARY KEY (id_livro),
   FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
 );
